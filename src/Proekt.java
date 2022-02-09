ca71ee7a80a9c4149ff1b937606b89e859a37042
we100ka@gmail.com <we100ka@yandex.by>
2022-02-09 21:38:11 +0300

    Initial commit

---
+++ src/Proekt.java
@@ -0,0 +1,10 @@
+public class Proekt {
+    public static void main(String[] args) {
+        //       System.out.println("Hello world");
+        int a = 10;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
+        dsdfsfs
+    }
+}
