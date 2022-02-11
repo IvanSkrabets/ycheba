72d1945b092572fdaad3613d597843746b40ab2e
we100ka@gmail.com <we100ka@yandex.by>
2022-02-11 21:16:59 +0300

    Ycheba

--- src/Ycheba.java
+++ src/Ycheba.java
@@ -1,7 +1,20 @@
+    import java.util.Scanner;
+
 public class Ycheba {
-    public static void main (String[] args ) {
-        System.out.println("Hello world");
-        int a = 20 ;
-        System.out.println( a );
+    public static void main(String[] args) {
+        int a = 0;
+        Scanner enter = new Scanner(System.in);
+        System.out.print("Введите число:");
+        int b = enter.nextInt();
+        if (b < 0) {
+            System.out.print("число отрицательное");
+        } else {
+            System.out.print("число положительное");
+        }
+        while (b > 0 || b < 0) {
+            b = b / 10;
+            a++;
+        }
+        System.out.print(" и количество цифр: " + a);
     }
 }
